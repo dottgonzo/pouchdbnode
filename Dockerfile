1633ad7b1c70e033edeb2409df0eb7084c2035f2
dario <dario@workstation.borgo33.caruso.online>
2019-10-10 01:42:32 +0200

    with ubuntu because of segfault

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM node:12-alpine
+FROM node:12
 USER root
-RUN apk add --no-cache curl nano tzdata
-RUN npm install -g pouchdb-server --ignore-scripts
+RUN apt update && apt install -y curl nano tzdata make python sqlite
+RUN npm_config_user=root npm install -g pouchdb-server
 WORKDIR /app
 CMD ./start.sh
 COPY ./start.sh ./
--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
+#!/bin/bash
+
 if [[ "$COUCHUSERNAME" ]]; then
 
-sleep 2
 
 pouchdb-server -m &
 
+sleep 4
 
 curl -XPUT "http://localhost:5984/_node/node1@127.0.0.1/_config/admins/$COUCHUSERNAME" -d "\"$COUCHPASSW\""
 
-sleep 4
-
-kill -9 $(ps aux | grep pouch| grep -v 'grep'|awk '{print($1)}')
+kill -9 $(ps aux | grep pouch| grep -v 'grep'|awk '{print($2)}')
 sleep 2
 
 fi
 
-pouchdb-server -o 0.0.0.0 -m
+pouchdb-server -o 0.0.0.0
